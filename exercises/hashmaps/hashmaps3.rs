e9095aff404f7b88631ad5584c29f296ae0a7f07
ISSuh <yoyus5@naver.com>
2023-01-07 17:38:29 +0900

    exercise 4

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,8 +14,6 @@
 
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 use std::collections::HashMap;
 
 // A structure to store team name and its goal details.
@@ -23,6 +21,20 @@ struct Team {
     name: String,
     goals_scored: u8,
     goals_conceded: u8,
+}
+
+fn match_and_intert(teams: &mut HashMap<String, Team>, team_name: String, score: u8, conceded: u8) {
+    let team = teams.get(&team_name);
+    match team {
+        Some(x) => {
+            let res_score = x.goals_scored + score;
+            let res_conceded = x.goals_conceded + conceded;
+            teams.insert(team_name.clone(), Team { name: team_name, goals_scored: res_score, goals_conceded: res_conceded });
+        },
+        None => {
+            teams.insert(team_name.clone(), Team { name: team_name, goals_scored: score, goals_conceded: conceded });
+        },
+    }
 }
 
 fn build_scores_table(results: String) -> HashMap<String, Team> {
@@ -40,6 +52,8 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        match_and_intert(&mut scores, team_1_name, team_1_score, team_2_score);
+        match_and_intert(&mut scores, team_2_name, team_2_score, team_1_score);
     }
     scores
 }
